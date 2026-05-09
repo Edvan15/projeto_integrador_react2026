2fec15b9e8e6570ddf22256a741ca7b9da98b9fe
Edvan <edvanmanfrin@gmail.com>
2026-05-09 21:51:32 +0000

    commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Atividade from './atividades/Atividade'
-import Aluno from './componentes/aluno'
-import Botao from './componentes/botao'
-import Titulo from './componentes/titulo'
-import Exercicio1 from './atividades/Exercicio1'
-import Exercicio2 from './atividades/Exercicio2'
+import BotoesDinamicos from './Atividade_06/05/2026/BotoesDinamicos'
+import CardAluno from './Atividade_06/05/2026/CardAluno'
+import CssInline from './Atividade_06/05/2026/CssInline'
+import GridProdutos from './Atividade_06/05/2026/GridProdutos'
+import ListaAlunos from './Atividade_06/05/2026/ListaAlunos'
+import ListaFilmes from './Atividade_06/05/2026/ListaFilmes'
+import ListaProdutos from './Atividade_06/05/2026/ListaProdutos'
+import ListaTarefas from './Atividade_06/05/2026/ListaTarefas'
+import MenuHorizontal from './Atividade_06/05/2026/MenuHorizontal'
+import TelaUsuarios from './Atividade_06/05/2026/TelaUsuarios'
+import VitrineLoja from './Atividade_06/05/2026/VitrineLoja'
+
 
 function App() {
 
   return (
     <>
       <div>
-          <Exercicio1 />
+        <BotoesDinamicos />
+        <CardAluno />
+        <CssInline />
+        <GridProdutos />
+        <ListaAlunos />
+        <ListaFilmes />
+        <ListaProdutos />
+        <ListaTarefas />
+        <MenuHorizontal />
+        <TelaUsuarios />
+        <VitrineLoja />
       </div>
     </>
   )
